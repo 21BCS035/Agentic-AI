062ae511e18c1db5b70164a3d763d8dbabe83130
Arpit Yadav <ar5136874yadav@gmail.com>
2026-01-08 15:29:52 +0530

    added output format

--- weather-agent/main.py
+++ weather-agent/main.py
@@ -4,6 +4,8 @@ import json
 from openai import OpenAI
 from dotenv import load_dotenv
 import requests
+from pydantic import BaseModel,Field
+from typing import Optional
 
 load_dotenv()
 
@@ -82,6 +84,12 @@ SYSTEM_PROMPT="""
    OUTPUT: { "step": "OUTPUT": "content": "The current weather of Bangalore is 24 C with some cloudy sky" }
 """
 
+class OutputFormat(BaseModel):
+    step: str = Field(...,description="The ID of the step.Example: PLAN, OUTPUT, TOOL, etc")
+    content: Optional[str] = Field(None,description="The optional string content for the step.")
+    input: Optional[str] = Field(None,description="The input params for the tool.")
+    tool: Optional[str] = Field(None,description="The ID of the tool to call.")
+
 message_history = [
     {"role": "system", "content": SYSTEM_PROMPT}
 ]
@@ -96,23 +104,23 @@ while True:
 
 
     while True:
-        response = client.chat.completions.create(
+        response = client.chat.completions.parse(
             model="gpt-4o-mini",
-            response_format={"type": "json_object"},
+            response_format=OutputFormat,
             messages=message_history
         )
 
         raw_result = response.choices[0].message.content
         message_history.append({"role":"assistant","content":raw_result})
-        parsed_result = json.loads(raw_result)
+        parsed_result = response.choices[0].message.parsed
 
-        if parsed_result.get("step") == "START":
-            print("🔥 ",parsed_result.get("content"))
+        if parsed_result.step == "START":
+            print("🔥 ",parsed_result.content)
             continue
 
-        if parsed_result.get("step") == "TOOL":
-            tool_to_call = parsed_result.get("tool")
-            tool_input = parsed_result.get("input")
+        if parsed_result.step == "TOOL":
+            tool_to_call = parsed_result.tool
+            tool_input = parsed_result.input
             print(f"🧰: {tool_to_call} ({tool_input})")
 
             tool_response = available_tools[tool_to_call](tool_input)
@@ -124,10 +132,10 @@ while True:
 
             
         
-        if parsed_result.get("step") == "PLAN":
-            print("🧠 ",parsed_result.get("content"))
+        if parsed_result.step == "PLAN":
+            print("🧠 ",parsed_result.content)
             continue
 
-        if parsed_result.get("step") == "OUTPUT":
-            print("🏢 ",parsed_result.get("content"))
+        if parsed_result.step == "OUTPUT":
+            print("🏢 ",parsed_result.content)
             break
