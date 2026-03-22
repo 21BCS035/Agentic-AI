be3454d3a4468c5e4188b609100040e3239efe4e
MainAdmin <mainadmin@M2Air.local>
2026-03-22 23:40:18 +0530

    implementation of rq server

--- rag/chat.py
+++ rag/chat.py
@@ -24,7 +24,7 @@ user_query = input("Ask something ...")
 search_results = vector_db.similarity_search(query=user_query)
 
 context = "\n\n\n".join([f"Page Content: {result.page_content}\nPage Number: {result.metadata['page_label']}\nFile Location: {result.metadata['source']}"
-for result in search_results])
+])
 
 SYSTEM_PROMPT = f"""
  You are an expert helpful AI Assistant who answers user query based on the available context retrieved from PDF file along with page_contents and page number.
